198ce473f4dc05b9b58d61fc2617b43933555ca5
j_web <65983437+ancho4@users.noreply.github.com>
2021-04-14 23:55:24 -0400

    edit

--- server.js
+++ server.js
@@ -1,28 +1,60 @@
+// These are our required libraries to make the server work.
+/* eslint-disable no-console */
+/* eslint-disable no-unused-vars */
+import path from 'path';
 import express from 'express';
-import db from './database/initializeDB.js';
-import apiRoutes from './routes/apiRoutes.js';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import reload from 'livereload';
+import connectReload from 'connect-livereload';
 
+dotenv.config();
+
+const __dirname = path.resolve();
 const app = express();
+const port = process.env.PORT || 3000;
 const staticFolder = 'public';
-const PORT = process.env.PORT || 3000;
 
+// Add some auto-reloading to our server
+const liveReloadServer = reload.createServer();
+liveReloadServer.watch(path.join(__dirname, staticFolder));
+
+// Configure express
+app.use(connectReload());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(staticFolder));
 
-app.use('/api', apiRoutes);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
-async function bootServer() {
-  try {
-    const mysql = await db.sequelizeDB;
-    await mysql.sync();
-    app.listen(PORT, () => {
-      console.log(`Listening on: http//localhost:${PORT}`);
-    });
-  } catch (err) {
-    console.error(err);
-  }
-}
+app.route('/api')
+  .get(async (req, res) => {
+    console.log('GET request detected');
+    const data = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
+    const json = await data.json();
+    console.log('data from fetch', json);
+    res.json(json);
+  })
+  .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+    console.log('Now send something back to your client');
+    // res.json({data: dataToSendToFrontEnd});
+  });
 
-bootServer();
+app.listen(port, async () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+liveReloadServer.server.once('connection', () => {
+  setTimeout(() => {
+    liveReloadServer.refresh('/');
+  }, 100);
+});
+
 
 
