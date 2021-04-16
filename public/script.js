78e764e83669b9d92ae39fa8910f515b5ffe592b
j_web <65983437+ancho4@users.noreply.github.com>
2021-04-16 16:18:53 -0400

    edit

--- public/script.js
+++ public/script.js
@@ -1,8 +1,31 @@
 async function windowActions() {
   const request = await fetch('/api/dining');
-  const data = await request.json();
-  document.getElementById("insert").innerHTML = data;
+  const data = await request.json(); 
+
+
+  //select table
+  const target = document.querySelector("tbody")
+  const table_row
+  // for each element in data
+   // const table_row = document.createElement("tr")
+  for (i = 0; i < data.length; i++) {
+    console.log("hello")
+   
+  }
+  table_row.innerHTML = "<td>hello</td>"
+  target.append(table_row)
+  
+
+ 
+  
+
+
+ 
+    
+ 
+
 }
+
 
 window.onload = windowActions
 
